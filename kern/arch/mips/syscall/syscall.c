7926c2b68ce2d634bd58260d5e9a1964dede3323
shannenbravo <shannenbravo@gmail.com>
2017-12-02 19:18:43 -0500

    BATTYHOLES!

--- kern/arch/mips/syscall/syscall.c
+++ kern/arch/mips/syscall/syscall.c
@@ -1,32 +1,3 @@
-/*
- * Copyright (c) 2000, 2001, 2002, 2003, 2004, 2005, 2008, 2009
- *	The President and Fellows of Harvard College.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE UNIVERSITY AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE UNIVERSITY OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- */
-
 #include <types.h>
 #include <kern/errno.h>
 #include <kern/syscall.h>
@@ -39,44 +10,6 @@
 #include <syscall.h>
 
 
-/*
- * System call dispatcher.
- *
- * A pointer to the trapframe created during exception entry (in
- * exception-*.S) is passed in.
- *
- * The calling conventions for syscalls are as follows: Like ordinary
- * function calls, the first 4 32-bit arguments are passed in the 4
- * argument registers a0-a3. 64-bit arguments are passed in *aligned*
- * pairs of registers, that is, either a0/a1 or a2/a3. This means that
- * if the first argument is 32-bit and the second is 64-bit, a1 is
- * unused.
- *
- * This much is the same as the calling conventions for ordinary
- * function calls. In addition, the system call number is passed in
- * the v0 register.
- *
- * On successful return, the return value is passed back in the v0
- * register, or v0 and v1 if 64-bit. This is also like an ordinary
- * function call, and additionally the a3 register is also set to 0 to
- * indicate success.
- *
- * On an error return, the error code is passed back in the v0
- * register, and the a3 register is set to 1 to indicate failure.
- * (Userlevel code takes care of storing the error code in errno and
- * returning the value -1 from the actual userlevel syscall function.
- * See src/user/lib/libc/arch/mips/syscalls-mips.S and related files.)
- *
- * Upon syscall return the program counter stored in the trapframe
- * must be incremented by one instruction; otherwise the exception
- * return code will restart the "syscall" instruction and the system
- * call will repeat forever.
- *
- * If you run out of registers (which happens quickly with 64-bit
- * values) further arguments must be fetched from the user-level
- * stack, starting at sp+16 to skip over the slots for the
- * registerized values, with copyin().
- */
 void
 syscall(struct trapframe *tf)
 {
@@ -89,16 +22,6 @@ syscall(struct trapframe *tf)
 	KASSERT(curthread->t_iplhigh_count == 0);
 
 	callno = tf->tf_v0;
-
-	/*
-	 * Initialize retval to 0. Many of the system calls don't
-	 * really return a value, just 0 for success and -1 on
-	 * error. Since retval is the value returned on success,
-	 * initialize it to 0 by default; thus it's not necessary to
-	 * deal with it except for calls that return other values,
-	 * like write.
-	 */
-
 	retval = 0;
 
 	switch (callno) {
@@ -133,8 +56,27 @@ syscall(struct trapframe *tf)
                         &retval);
                 break;
 
-            /* Project 3:
-             * Add more for meld, close, and write . */
+             case SYS_write:
+                err = sys_write(
+                        tf->tf_a0,
+                        (userptr_t)tf->tf_a1,
+                        tf->tf_a2,
+                        &retval);
+                break;
+
+             case SYS_close:
+                err = sys_close(
+                        tf->tf_a0,
+                        &retval);
+                break;
+
+             case SYS_meld:
+                err = sys_meld(
+                        (userptr_t)tf->tf_a0,
+                        (userptr_t)tf->tf_a1,
+                        (userptr_t)tf->tf_a2,
+                        &retval);
+                break;
 
 	    default:
 		kprintf("Unknown syscall %d\n", callno);
